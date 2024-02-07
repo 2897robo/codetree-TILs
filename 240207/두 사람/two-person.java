6197a6581b523d3aef812485e3ac552007185586
Kim Ki Ug <2897robo@naver.com>
2024-02-07 18:03:36 +1000

    Title: 두 사람, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240207/두 사람/two-person.java
+++ 240207/두 사람/two-person.java
@@ -6,13 +6,13 @@ public class Main {
 
         // 첫 번째 쌍 입력
         int firstAge = sc.nextInt();
-        char firstGender = sc.next().charAt(0);
+        String firstGender = sc.next();
 
         // 두 번째 쌍 입력
         int secondAge = sc.nextInt();
-        char secondGender = sc.next().charAt(0);
+        String secondGender = sc.next();
 
         // 조건을 만족하는 경우 "1", 아닌 경우 "0" 출력
-        System.out.print((firstAge >= 19 || secondAge >= 19) && (firstGender == 'M' || secondGender == 'M') ? "1" : "0");
+        System.out.print((firstAge >= 19 || secondAge >= 19) && (firstGender.equals("M") || secondGender.equals("M")) ? "1" : "0");
     }
 }
