e6782afb2b4105dce60ee07e10a6bb32b45e1789
Kim Ki Ug <2897robo@naver.com>
2024-02-20 17:48:04 +0900

    Title: 별표 출력하기 13, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 240220/별표 출력하기 13/print-start-13.java
+++ 240220/별표 출력하기 13/print-start-13.java
@@ -1,25 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int k = 0;
 
-        for(int i =0; i<n; i++) {
-            for(int j =0; j<n; j++){
-                if(i == 0) System.out.print("* ");
-                else if(i == 1 && j == 0) System.out.print("* ");
-                else if (i > 1 && n - i >= j) System.out.print("* ");
+        for(int i=n; i>0; i--) {
+            for(int j=0; j<i; j++) {
+                System.out.print("* ");
             }
             System.out.println();
-        }
-        for(int i =n-1; i>=0; i--) {
-            for(int j =0; j<n; j++){
-                if(i == 0) System.out.print("* ");
-                else if(i == 1 && j == 0) System.out.print("* ");
-                else if (i > 1 && n - i >= j) System.out.print("* ");
+            for(int j=0; j<=k; j++) {
+                System.out.print("* ");
             }
             System.out.println();
+            k++;
+            if(k==n)
+                break;
         }
     }
 }
