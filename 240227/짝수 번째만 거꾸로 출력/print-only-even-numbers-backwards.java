3a3f1b95583f5fef56d95b7b4a3d7599a94af511
Kim Ki Ug <2897robo@naver.com>
2024-02-27 08:51:29 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 240227/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
+++ 240227/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
@@ -5,9 +5,14 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         String c = sc.nextLine();
+        String s = "";
+        
+        for(int i=1; i<c.length(); i+=2) {
+            s += c.charAt(i);
+        }
 
-        for(int i=c.length()-1; i>=0; i=i-2) {
-            System.out.print(c.charAt(i));
+        for(int i=s.length()-1; i>=0; i--) {
+            System.out.print(s.charAt(i));
         }
     }
 }
