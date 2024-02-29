cd3235117280c732d8e8b97085ad2d0b4d6e0b03
Kim Ki Ug <2897robo@naver.com>
2024-02-29 09:55:01 +0900

    Title: 미는 횟수, Time: 789ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240229/미는 횟수/number-of-pushes.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+
+        System.out.print(StrShift(a,b));
+    }
+
+    public static int StrShift(String a, String b) {
+        int cnt = 0;
+        
+        while(true) {
+            a = a.charAt(a.length()-1) + a.substring(0, a.length()-1);
+            cnt++;
+            if(a.equals(b)) {
+                break;
+            }
+        }
+
+        return cnt;
+    }
+}
