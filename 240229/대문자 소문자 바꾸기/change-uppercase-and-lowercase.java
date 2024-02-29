43c778430896f4aa082b1106e516c92fa014cdd2
Kim Ki Ug <2897robo@naver.com>
2024-02-29 09:12:51 +0900

    Title: 대문자 소문자 바꾸기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240229/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+
+        StringBuilder result = new StringBuilder();
+
+        for (char c : a.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                result.append(Character.toLowerCase(c));
+            } else if (Character.isLowerCase(c)) {
+                result.append(Character.toUpperCase(c));
+            }
+        }
+
+        System.out.print(result.toString());
+    }
+}
