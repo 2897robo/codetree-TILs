a01af050a3f418e2a9f60cd88ee0980232d93968
Kim Gi Uk <2897robo@naver.com>
2024-07-20 21:12:36 +0900

    Title: 일치하는 문자열의 수, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240720/일치하는 문자열의 수/num-of-correct-string.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String answer = sc.next();
+        
+        String[] tmp = new String[n];
+        int cnt = 0;
+        for(int i=0; i<n; i++) {
+            tmp[i] = sc.nextLine();
+            if(answer.equals(tmp[i])) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
